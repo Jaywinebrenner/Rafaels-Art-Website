8dc7e7fcc4f835f2b5fd001644ea2cac740890b8
Jay Winebrenner <jaywinebrenner@gmail.com>
2021-04-08 09:53:31 -0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,87 @@
-$(function() {
+// Animate text upon hovering over logo
+$( ".bioLinkWrapper" ).hover(
+    function() {
+      $( ".bioLogoText" ).addClass( "showText" );
+    }, function() {
+      $( ".bioLogoText" ).removeClass( "showText" );
+    }
+  );
 
-	// Cache the Window object
-	var $window = $(window);
-	
-	// Parallax Backgrounds
-	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
-	
-	$('section[data-type="background"]').each(function(){
-		var $bgobj = $(this); // assigning the object
-		
-		$(window).scroll(function() {
-		
-			// Scroll the background at var speed
-			// the yPos is a negative value because we're scrolling it UP!								
-			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-			
-			// Put together our final background position
-			var coords = '50% '+ yPos + 'px';
-			
-			// Move the background
-			$bgobj.css({ backgroundPosition: coords });
-			
-		}); // end window scroll
-	});
-	
+  $( ".workLinkWrapper" ).hover(
+    function() {
+      $( ".workLogoText" ).addClass( "showText" );
+    }, function() {
+      $( ".workLogoText" ).removeClass( "showText" );
+    }
+  );
+
+  $( ".showsLinkWrapper" ).hover(
+    function() {
+      $( ".showsLogoText" ).addClass( "showText" );
+    }, function() {
+      $( ".showsLogoText" ).removeClass( "showText" );
+    }
+  );
+
+  $( ".contactLinkWrapper" ).hover(
+    function() {
+      $( ".contactLogoText" ).addClass( "showText" );
+    }, function() {
+      $( ".contactLogoText" ).removeClass( "showText" );
+    }
+  );
+
+
+
+
+
+// Links for Home Page Logos
+
+$('.bioLinkWrapper').click(function () {
+    window.location = 'bio.html';
 });
+
+$('.workLinkWrapper').click(function () {
+    window.location = 'work.html';
+});
+
+$('.showsLinkWrapper').click(function () {
+    window.location = 'shows.html';
+});
+
+$('.contactLinkWrapper').click(function () {
+    window.location = 'contact.html';
+});
+
+
+
+
+
+
+// $(function() {
+
+
+// 	var $window = $(window);
+	
+// 	// Parallax Backgrounds
+// 	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
+	
+// 	$('section[data-type="background"]').each(function(){
+// 		var $bgobj = $(this); // assigning the object
+		
+// 		$(window).scroll(function() {
+		
+// 			// Scroll the background at var speed
+// 			// the yPos is a negative value because we're scrolling it UP!								
+// 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			
+// 			// Put together our final background position
+// 			var coords = '50% '+ yPos + 'px';
+			
+// 			// Move the background
+// 			$bgobj.css({ backgroundPosition: coords });
+			
+// 		}); // end window scroll
+// 	});
+	
+// });
